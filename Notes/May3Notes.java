3dc4f7d96f97f9417a12063a49effcaa9e3723dc
jmadhu <jmadhu@princeton.edu>
2013-05-03 15:47:28 -0400

    notes

--- Notes/May3Notes.java
+++ Notes/May3Notes.java
@@ -1,12 +1,13 @@
 Meeting minutes:
 1. Argument: add path to text files
 2. Argument: add eviction method
-2. map cache to eviction schemes
-3. Add eviction strategies to cache
-4. Collect data from existing simulator
-5. Collect data from eviction methods
-6. experiments: different cache sizes and chunk sizes.
-7. experiment: using different mobile devices
-8. update readme with all this information
-9. misrate: how many bytes were redundant.. (distinct)
+3. Argument: cache size
+4. map cache to eviction schemes
+5. Add eviction strategies to cache
+6. Collect data from existing simulator
+7. Collect data from eviction methods
+8. experiments: different cache sizes and chunk sizes.
+9. experiment: using different mobile devices
+10. update readme with all this information
+11. misrate: how many bytes were redundant.. (distinct)
 
