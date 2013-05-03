887dd9e0b1b1eb3e63f71ab7e5d5e791a579d28b
jmadhu <jmadhu@princeton.edu>
2013-05-03 15:57:06 -0400

    notes

--- Notes/May3Notes.java
+++ Notes/May3Notes.java
@@ -1,13 +1,18 @@
 Meeting minutes:
-1. Argument: add path to text files
-2. Argument: add eviction method
-3. Argument: cache size
-4. map cache to eviction schemes
-5. Add eviction strategies to cache
-6. Collect data from existing simulator
-7. Collect data from eviction methods
-8. experiments: different cache sizes and chunk sizes.
-9. experiment: using different mobile devices
-10. update readme with all this information
-11. misrate: how many bytes were redundant.. (distinct)
+1. Argument: add path to text files - Nayden
+2. Argument: add eviction method - Nayden
+3. Argument: cache size - Nayden
+4. map cache to eviction schemes - Nayden
+5. Add eviction strategies to cache - Nayden
+6. Collect data from existing simulator - Madhu
+7. Collect data from eviction methods - Madhu
+8. experiments: different cache sizes and chunk sizes. - Madhu
+9. experiment: using different mobile devices (add mobile variables in simulator)- Madhu
+10. update readme with all this information - Madhu
+11. misrate: how many bytes were redundant.. (distinct) - Madhu
+12. Readme for Proxy - Madhu
+13. Networked simulation -Marcela 
+14. Upload client - Madhu
+15. Create Proxy simulator (similar to Mobile simulator). 
 
+Next Meeting: Monday, May 6: 3pm
