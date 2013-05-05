75d22bac0d764766f15b5f652fa9bbd7d8e4d0c5
Marcela <msmelara@gmail.com>
2013-05-05 17:36:02 -0400

    Added arguments to simulator

--- java/simulation/Main.java
+++ java/simulation/Main.java
@@ -11,23 +11,31 @@ public class Main {
 		// Enter chunk size as argument when running the Simulator for stats purposes!
 		int chunk_size = 0;
 		String path = "";
+		String website = "";
+		int proxyCacheSize = 0;
+		int mobileCacheSize = 0;
+		int numIters = 0;
 
-		if(args.length != 2){
-		    System.out.println("Format: java SimulatorV1 [string: path] [postive int: chunk_size]");
+		if(args.length != 6){
+		    System.out.println("Format: java SimulatorV1 [str: path-to-packet_bytes] [str: website] [+ int: chunk_size] [+ int: proxy cache size in chunks] [+ int: mobile cache size in chunks] [+ int: num iterations]");
 		    System.exit(-1);
 		}
 
 		path = args[0];
+		website = args[1];
 		try {
-		    chunk_size = Integer.parseInt(args[1]);
+		    chunk_size = Integer.parseInt(args[2]);
+		    proxyCacheSize = Integer.parseInt(args[3]);
+		    mobileCacheSize = Integer.parseInt(args[4]);
+		    numIters = Integer.parseInt(args[5]);
 		}
 		catch(NumberFormatException e){
-		    System.out.println("Chunk size must be positive integer!!!!");
+		    System.out.println("All numbers must be positive integers!!!!");
 		    System.exit(-1);
 		}
 		
 		// create a new simulator with the desired implementation
-		ISimulator simulator = new SimulatorV1(path, chunk_size);
+		ISimulator simulator = new SimulatorV1(path, website, chunk_size, proxyCacheSize, mobileCacheSize, numIters);
 		simulator.simulate();
 	}
 
--- java/simulation/SimulatorV1.java
+++ java/simulation/SimulatorV1.java
@@ -18,31 +18,39 @@ import chunking.Chunking;
 public class SimulatorV1 implements ISimulator {
 
 	private String path;
+	private String website;
 	private int chunk_size;
+	private int proxyCacheSize;
+	private int mobileCacheSize;
+	private int numIters;
 
-	public SimulatorV1(String path, int chunk_size) {
+	public SimulatorV1(String path, String website, int chunk_size, int proxyCacheSize, int mobileCacheSize, int numIters) {
 		this.path = path;
+		this.website = website;
 		this.chunk_size = chunk_size;
+		this.proxyCacheSize = proxyCacheSize;
+		this.mobileCacheSize = mobileCacheSize;
+		this.numIters = numIters;
 	}
 
 	@Override
 	public void simulate() {
 		// The Chunking Facility
-		Chunking chunking = new Chunking(path + "/packet_bytes/amazon1.txt", chunk_size);
+		Chunking chunking = new Chunking(path+"/packet_bytes/"+website+"1.txt", chunk_size); //assume that we always want to start analyzing at 1st page
 
 		// A proxy server
-		ProxyServer proxy = new ProxyServer(chunk_size*2000); //holds 2000 chunks; TODO: this should be an argument
+		ProxyServer proxy = new ProxyServer(proxyCacheSize); //holds 2000 chunks; TODO: this should be an argument
 
 		// A mobile device; Could imagine simulating multiple devices
-		Mobile mobile = new Mobile(chunk_size*500); //holds 500 chunks; TODO: this should be an argument
+		Mobile mobile = new Mobile(mobileCacheSize); //holds 500 chunks; TODO: this should be an argument
 
 		// Used to calculated the average missrates
 		double proxySum = 0;
 		double mobSum = 0;
 
 		// loop over all files of a given website and look at stats
-		for(int i = 1; i <= 8; i++) { // Caution: Hardcoded!
-		    chunking.reset(path + "/packet_bytes/amazon" + i + ".txt");
+		for(int i = 1; i <= numIters; i++) { // Caution: Hardcoded!
+		    chunking.reset(path +"/packet_bytes/" + website + i + ".txt");
 
 		    System.out.println("Processing file: " + chunking.getFilename());
 
@@ -76,10 +84,7 @@ public class SimulatorV1 implements ISimulator {
 		    mobile.receiveFps(fps);
 
 		    // System.out.println("Mobile device is checking its cache for the fingerprints it received from the proxy server.");
-
-		   // proxyCache.processWebContent(chunks);
-		//    proxy.prepareData(chunks);
-		    
+    
 		    // Mobile device checks its cache for the fps it received from the proxy
 		    ArrayList<Integer> mobNeeded = mobile.sendNeededFps();
 		    
@@ -116,9 +121,9 @@ public class SimulatorV1 implements ISimulator {
 		    mobSum += mobile.getCache().getMissRate();		
 			// Hardcoded number of sites!
 			System.out.print("Avg proxy missrate over all inspected sites: ");
-			customFormat("##.####", proxySum/8);
+			customFormat("##.####", proxySum/numIters);
 			System.out.print("Avg mobile missrate over all inspected sites: ");
-			customFormat("##.####", mobSum/8);
+			customFormat("##.####", mobSum/numIters);
 		} 
 	}
         
