a7bd9ee65d3fe30cc19d9c643ac6fe0d2c97b984
Marcela <msmelara@gmail.com>
2013-05-09 14:27:33 -0400

    Fixed networked proxy server thread bug

--- java/javaproxy/ProxyServerNet.java
+++ java/javaproxy/ProxyServerNet.java
@@ -17,9 +17,6 @@ public class ProxyServerNet{
     
     private static IProcessor proc;
     private static ICache cache;
-    private static ArrayList<Chunk> allChunks;
-    private static ArrayList<Integer> allFps;
-    private static ArrayList<Integer> neededFps;
     private static int chunkSize;
 
     public static ICache getCache(){
@@ -38,139 +35,6 @@ public class ProxyServerNet{
 	chunkSize = cSize;
 	cache = new MRUCache(size); // Server cache holds size chunks
 	proc = new SimpleProcessor();
-	allChunks = new ArrayList<Chunk>();
-	allFps = new ArrayList<Integer>();
-	neededFps = new ArrayList<Integer>();
-    }
-
-    /** Given an inputstream and a chunk size, generates chunks of the incoming data.
-     * @param in the InputStream which is sending the incoming data
-     * @param chunkSize the number of bytes each chunk contains
-     * @return a list of all the chunked data
-     */
-    private static ArrayList<Chunk> chunkWebData(InputStream in, int chunkSize){
-    	ArrayList<Chunk> chunks = new ArrayList<Chunk>();
-    	ChunkingNet.setStream(in);
-    	ChunkingNet.setChunkSize(chunkSize);
-	    // read next chunks while the end of the file has not been reached
-	    while(!ChunkingNet.isEOF()) {
-	    	try {
-	    		Chunk c = ChunkingNet.getNextChunk();
-
-			// ensure that we don't add empty chunk to our list
-			if(ChunkingNet.isEOF()){
-			    break;
-			}
-			chunks.add(c);
-	    	} catch (IOException ioe) {
-		    ioe.printStackTrace();
-	    	}
-	    }
-	    return chunks;
-    }
-
-    /** Given a list of chunks, get the fingerprint of each chunk. This will be used to send
-     * the fingerprints to the mobile cache, so the mobile device can check its cache for the
-     * pages.
-     */
-    private static ArrayList<Integer> getFingerprints(ArrayList<Chunk> content){
-	ArrayList<Integer> fps = new ArrayList<Integer>();
-
-	for(Chunk chunk : content){
-	    int fp = Fingerprinting.fingerprint(chunk.getData());
-	    fps.add(fp);
-	}
-
-	return fps;
-    } //ends getFingerprints()
-
-    /** Given the list of all web data chunks and the list of the needed fingerprints,
-     * create a list of chunks to be sent over to the mobile device. A null entry indicates that
-     * the mobile device already has this chunk in its cache.
-     */
-    private static ArrayList<Chunk> prepareData(ArrayList<Integer> neededFps){
-	
-	// Update cache before preparing the data for the mobile device
-	proc.processWebContent(allChunks, cache);
-	
-	ArrayList<Chunk> prepData = new ArrayList<Chunk>();
-	
-	if(allChunks.size() != neededFps.size()){
-	    System.out.println("Content and neededFps are not of the same length!");
-	    return null;
-	}
-	
-	for(int i = 0; i < allChunks.size(); i++){
-	    
-	    // check first to see if mobile device needs this chunk
-	    if(neededFps.get(i) == null){
-		prepData.add(null);
-	    }
-	    else{
-		int curFp = neededFps.get(i);
-		// check to see if we already have this chunk in our cache and if the mobile device needs it
-		if(cache.get(curFp) != null){
-		    prepData.add((Chunk)cache.get(curFp));
-		}
-		else if(cache.get(curFp) == null){
-		    prepData.add((Chunk)allChunks.get(i));
-		}
-	    }	    
-	    
-	}
-
-	return prepData;
-	
-    } //ends prepareData()
-
-    /** Sends an http GET request through the given outputstream to the given URL
-     * @param o the outpustream through which to send the GET request
-     * @param u the URL to reach with the request
-     */
-    private static void sendGetRequest(PrintStream o, URL u){
-	o.println("GET " +u+ " HTTP/1.1"); 
-	o.println("Host: " +u.getHost());
-	o.println("User-Agent: Mozilla/5.0 (Linux; U; Android 2.3; xx-xx; GT-I9100 Build/GRH78) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1");
-	o.println("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-	o.println("Accept-Language: en-US,en;q=0.5");
-	o.println("Connection: keep-alive");
-	o.println();
-	o.flush();
-    } //ends sendGetRequest()
-
-    private static void sendAllFps(PrintStream w, URL url){
-	w.println("ALL FPS "+url);
-	w.println(allFps.size());
-	
-	for(Integer i : allFps){
-	    w.println(i+"");
-	}
-	w.println();
-	w.flush();
-
-    } //ends sendAllFps()
-
-    /** Sends the needed chunks data to the mobile client
-     * @param o the outputstream through which to send the chunk data
-     * @param u the url of the webpage the chunks belong to
-     */
-    private static void sendNeededChunks(ArrayList<Chunk> needed, OutputStream o, URL u)
-    throws IOException {
-	String header = "NEEDED CHUNKS "+u;
-	o.write(header.getBytes());
-
-	for(Chunk c : needed){
-	    if(c == null){
-		byte [] nullByte = new byte[ChunkingNet.getChunkSize()];
-		o.write(nullByte);
-	    }
-	    else{
-		byte[] data = c.getData();
-		o.write(c.getData());
-	    }
-	}
-	o.write((int)'\n');
-	o.flush();
     }
 
     /** MAIN
@@ -251,6 +115,10 @@ public class ProxyServerNet{
 	private Socket clientSocket;
 
 	private final int PORT = 80; // All requests are made to this port
+
+	private ArrayList<Chunk> allChunks;
+	private ArrayList<Integer> allFps;
+	private ArrayList<Integer> neededFps;
 	
 	/** Constructor of a ServerThread
 	 * 
@@ -265,6 +133,11 @@ public class ProxyServerNet{
 	 * @param s the client socket
 	 */
 	public void run(){
+
+	    // initialize the lists
+	    allChunks = new ArrayList<Chunk>();
+	    allFps = new ArrayList<Integer>();
+	    neededFps = new ArrayList<Integer>();
 	    
 	    //attempt connection to the client
 	    try{			
@@ -423,6 +296,138 @@ public class ProxyServerNet{
 	    }
 	    
 	} //ends run()	
+
+	/** Given an inputstream and a chunk size, generates chunks of the incoming data.
+	 * @param in the InputStream which is sending the incoming data
+	 * @param chunkSize the number of bytes each chunk contains
+	 * @return a list of all the chunked data
+	 */
+	private ArrayList<Chunk> chunkWebData(InputStream in, int chunkSize){
+	    ArrayList<Chunk> chunks = new ArrayList<Chunk>();
+	    ChunkingNet.setStream(in);
+	    ChunkingNet.setChunkSize(chunkSize);
+	    // read next chunks while the end of the file has not been reached
+	    while(!ChunkingNet.isEOF()) {
+	    	try {
+		    Chunk c = ChunkingNet.getNextChunk();
+		    
+		    // ensure that we don't add empty chunk to our list
+		    if(ChunkingNet.isEOF()){
+			break;
+		    }
+		    chunks.add(c);
+	    	} catch (IOException ioe) {
+		    ioe.printStackTrace();
+	    	}
+	    }
+	    return chunks;
+	}
+	
+	/** Given a list of chunks, get the fingerprint of each chunk. This will be used to send
+	 * the fingerprints to the mobile cache, so the mobile device can check its cache for the
+	 * pages.
+	 */
+	private ArrayList<Integer> getFingerprints(ArrayList<Chunk> content){
+	    ArrayList<Integer> fps = new ArrayList<Integer>();
+	    
+	    for(Chunk chunk : content){
+		int fp = Fingerprinting.fingerprint(chunk.getData());
+		fps.add(fp);
+	    }
+	    
+	    return fps;
+	} //ends getFingerprints()
+	
+	/** Given the list of all web data chunks and the list of the needed fingerprints,
+	 * create a list of chunks to be sent over to the mobile device. A null entry indicates that
+	 * the mobile device already has this chunk in its cache.
+	 */
+	private ArrayList<Chunk> prepareData(ArrayList<Integer> neededFps){
+	
+	    // Update cache before preparing the data for the mobile device
+	    proc.processWebContent(allChunks, cache);
+	    
+	    ArrayList<Chunk> prepData = new ArrayList<Chunk>();
+	    
+	    if(allChunks.size() != neededFps.size()){
+		System.out.println(allChunks.size());
+		System.out.println(neededFps.size());
+		System.out.println("Content and neededFps are not of the same length!");
+		return null;
+	    }
+	    
+	    for(int i = 0; i < allChunks.size(); i++){
+		
+		// check first to see if mobile device needs this chunk
+		if(neededFps.get(i) == null){
+		    prepData.add(null);
+		}
+		else{
+		    int curFp = neededFps.get(i);
+		    // check to see if we already have this chunk in our cache and if the mobile device needs it
+		    if(cache.get(curFp) != null){
+			prepData.add((Chunk)cache.get(curFp));
+		    }
+		    else if(cache.get(curFp) == null){
+			prepData.add((Chunk)allChunks.get(i));
+		    }
+		}	    
+		
+	    }
+	    
+	    return prepData;
+	    
+	} //ends prepareData()
+	
+	/** Sends an http GET request through the given outputstream to the given URL
+	 * @param o the outpustream through which to send the GET request
+	 * @param u the URL to reach with the request
+	 */
+	private void sendGetRequest(PrintStream o, URL u){
+	o.println("GET " +u+ " HTTP/1.1"); 
+	o.println("Host: " +u.getHost());
+	o.println("User-Agent: Mozilla/5.0 (Linux; U; Android 2.3; xx-xx; GT-I9100 Build/GRH78) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1");
+	o.println("Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+	o.println("Accept-Language: en-US,en;q=0.5");
+	o.println("Connection: keep-alive");
+	o.println();
+	o.flush();
+	} //ends sendGetRequest()
+	
+	private void sendAllFps(PrintStream w, URL url){
+	    w.println("ALL FPS "+url);
+	    w.println(allFps.size());
+	    
+	    for(Integer i : allFps){
+		w.println(i+"");
+	    }
+	    w.println();
+	    w.flush();
+	    
+	} //ends sendAllFps()
+	
+	/** Sends the needed chunks data to the mobile client
+	 * @param o the outputstream through which to send the chunk data
+	 * @param u the url of the webpage the chunks belong to
+	 */
+	private void sendNeededChunks(ArrayList<Chunk> needed, OutputStream o, URL u)
+	    throws IOException {
+	    String header = "NEEDED CHUNKS "+u;
+	    o.write(header.getBytes());
+	    
+	    for(Chunk c : needed){
+		if(c == null){
+		    byte [] nullByte = new byte[ChunkingNet.getChunkSize()];
+		    o.write(nullByte);
+		}
+		else{
+		    byte[] data = c.getData();
+		    o.write(c.getData());
+		}
+	    }
+	    o.write((int)'\n');
+	    o.flush();
+	} //ends sendNeededChunks()
 	
     } //ends ServerThread class
     
